3cc245fb7c5c657f7a3fad5ac1e53c970d03b43d
Apocalypsor <37431235+Apocalypsor@users.noreply.github.com>
2022-01-14 23:19:23 -0500

    Create ipv6.sh

---
+++ dist/oracle/ipv6.sh
@@ -0,0 +1,24 @@
+apt update
+apt install curl wget sudo screen net-tools -y
+
+read -p "Enter Interface: " interface
+read -p "Enter IPv6 Address: " ipv6
+
+cat <<EOT >> /etc/init.d/ipv6
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides: ipv6
+# Required-Start: $network $remote_fs $local_fs
+# Required-Stop: $network $remote_fs $local_fs
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: ipv6
+# Description: ipv6
+### END INIT INFO
+
+ifconfig $interface inet6 add $ipv6/64 up
+
+exit 0
+EOT
+
+update-rc.d ipv6 defaults
