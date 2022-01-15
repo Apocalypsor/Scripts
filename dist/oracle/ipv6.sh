268ec6ccd520b83823c395ce9ba84309acd24cca
Apocalypsor <37431235+Apocalypsor@users.noreply.github.com>
2022-01-14 23:41:02 -0500

    Update ipv6.sh

--- dist/oracle/ipv6.sh
+++ dist/oracle/ipv6.sh
@@ -10,8 +10,8 @@ cat <<EOT > /etc/init.d/ipv6
 #!/bin/sh
 ### BEGIN INIT INFO
 # Provides: ipv6
-# Required-Start: $network $remote_fs $local_fs
-# Required-Stop: $network $remote_fs $local_fs
+# Required-Start: \$network \$remote_fs \$local_fs
+# Required-Stop: \$network \$remote_fs \$local_fs
 # Default-Start: 2 3 4 5
 # Default-Stop: 0 1 6
 # Short-Description: ipv6
