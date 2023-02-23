f4e19605200ccfa4bb9740bb8f188f13ef2b5dc6
Apocalypsor <chthon605@gmail.com>
2023-02-23 18:26:20 -0500

    Update docker.sh

--- dist/linux/docker.sh
+++ dist/linux/docker.sh
@@ -1,6 +1,5 @@
 apt install sudo wget screen -y
 curl -fsSL get.docker.com | sh
-apt install docker-compose -y
 sudo usermod -aG docker $USER
 sudo systemctl enable docker
 sudo systemctl start docker
